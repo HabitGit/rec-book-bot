a44120ac30d4706efa04c538eda83081ea9aa331
Igor <char.habit@gmail.com>
2023-09-05 17:43:42 +0400

    init genres repository; init books repository; init authors repository;

---
+++ database/src/repository/books.repository.ts
@@ -0,0 +1,31 @@
+import { DataSource, Repository } from 'typeorm';
+import { Books } from '../entitys/books.entity';
+import { CreateBookDto, UpdateBookDto } from '../dto/books.dto';
+
+export class BooksRepository extends Repository<Books> {
+  constructor(dataSource: DataSource) {
+    super(Books, dataSource.createEntityManager());
+  }
+
+  async createBook(bookData: CreateBookDto): Promise<Books> {
+    const isBook: Books | null = await this.findOne({
+      where: { litresId: bookData.litresId },
+    });
+    if (isBook) throw new Error('Такая книга уже существует');
+    return this.save(bookData);
+  }
+
+  async updateBook(updateBookData: UpdateBookDto) {
+    const isBook: Books | null = await this.findOne({
+      relations: { authors: true },
+      where: { litresId: updateBookData.litresId },
+    });
+    if (!isBook) throw new Error('Такой книги нету');
+    return this.save({
+      ...updateBookData,
+      authors: updateBookData.authors
+        ? [...isBook.authors, ...updateBookData.authors]
+        : [...isBook.authors],
+    });
+  }
+}
