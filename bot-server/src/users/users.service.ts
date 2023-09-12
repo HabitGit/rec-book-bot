7487bfc79d68347705f29dbb39d678ad0a61e374
Habit <char.habit@gmail.com>
2023-09-12 19:58:31 +0400

    feat: create user service; feat: create message controller; feat: create bot service;

---
+++ bot-server/src/users/users.service.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_LINK = 'http://localhost:3000';
+
+export class UsersService {
+  async checkUser(userId: number) {
+    try {
+      const isUser = await axios.post(`${API_LINK}/users/profile/${userId}`, {
+        type: 'profile',
+      });
+      if (isUser) {
+        return true;
+      }
+    } catch (e) {
+      if (
+        axios.isAxiosError(e) &&
+        e.message === 'Request failed with status code 401'
+      ) {
+        return false;
+      } else {
+        console.log('[-]*UsersService*CheckUser*Неизвестная ошибка: ', e);
+        throw new Error(`Неизвестная ошибка: ${e}`);
+      }
+    }
+  }
+}
