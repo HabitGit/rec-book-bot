f01c84d4b804869120e966b6c50ce81a2ee3a5ec
Habit <char.habit@gmail.com>
2023-09-12 16:31:51 +0400

    fix: add path to env in bot - module;

--- bot-server/src/index.ts
+++ bot-server/src/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config';
 import TelegramBot from 'node-telegram-bot-api';
+import { config } from 'dotenv';
+import * as path from 'path';
+
+const envPath: string = path.join(__dirname + '/../../.env');
+config({
+  path: envPath,
+});
 
 const TOKEN: string | undefined = process.env.TOKEN;
 if (!TOKEN) throw new Error('Нету токена');
@@ -7,5 +13,5 @@ if (!TOKEN) throw new Error('Нету токена');
 const Bot: TelegramBot = new TelegramBot(TOKEN, { polling: true });
 
 Bot.on('message', async (msg) => {
-    await Bot.sendMessage(msg.chat.id, 'Hello!');
-})
+  await Bot.sendMessage(msg.chat.id, 'Hello!');
+});
